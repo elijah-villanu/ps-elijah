45ba3ae918c145b0d5fdf376bfd89b641dcbff6a
elijah-villanu <elijahred9@outlook.com>
2026-07-06 12:01:07 -0700

    Update Bio

--- my-portfolio/src/App.tsx
+++ my-portfolio/src/App.tsx
@@ -7,7 +7,7 @@ import type { Page } from "./types";
 
 const about: string = `Welcome! I'm Elijah Villanueva, and I'm a senior from California Polytechnic State University, San Luis Obispo! I'm expected to get my Bachelor's Degree in Computer Science this August 2026. I'm passionate about game developement, software engineering (yes, I own Designing Data-Intensive Applications by  Martin Kleppman), and computer graphics; but honestly, anything tech gets me excited!
   
-  Currently, I work at Cal Poly as a UI/UX Research Assistant under Dr. Silas Hsu. Currently, I work on two of Dr. Hsu's projects: 'How Does User Control Reduce Irritation of Mid-Roll Ads' and 'How Does Gamified Rhythm Practice Affect Performance'. On another note, I'm also working on getting my AZ-900 certificate for Azure.
+  Currently, I work at Cal Poly as a UI/UX Research Assistant under Dr. Silas Hsu. Currently, I work on two of Dr. Hsu's projects: 'How Does User Control Reduce Irritation of Mid-Roll Ads' and . On another note, I'm also working on getting my AZ-900 certificate for Azure.
 
   On my free time, I play video games, make LoFi Beats, and play the drum set!
   `
